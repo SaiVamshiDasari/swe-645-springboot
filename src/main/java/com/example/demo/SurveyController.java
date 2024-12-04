f4dac1ccb7e5eadf773b21f6486bc5cd2af4486f
sunny <saivamshidasari1432@gmail.com>
2024-12-04 18:08:36 -0500

    6th commit

--- src/main/java/com/example/demo/SurveyController.java
+++ src/main/java/com/example/demo/SurveyController.java
@@ -6,13 +6,10 @@
  * Lahari ummadisetty-G01454186
  */
 package com.example.demo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/survey")
