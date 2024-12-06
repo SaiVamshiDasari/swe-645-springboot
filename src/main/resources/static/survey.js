3733ff854765d238ff2ddda0358c3e6b517a9b7f
sunny <saivamshidasari1432@gmail.com>
2024-12-05 19:30:18 -0500

    6th commit

--- src/main/resources/static/survey.js
+++ src/main/resources/static/survey.js
@@ -7,7 +7,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     // Define your backend base URL
-    const BASE_URL = "http://acd202bc46cc9407cb4f97a7021422c3-71503224.us-east-1.elb.amazonaws.com/"; // Change this to your deployed backend URL if needed
+    const BASE_URL = "http://acd202bc46cc9407cb4f97a7021422c3-71503224.us-east-1.elb.amazonaws.com"; // Change this to your deployed backend URL if needed
 
     // Handle form submission
     const surveyForm = document.querySelector("form");
