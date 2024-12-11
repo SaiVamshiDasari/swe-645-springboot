8b250ab5d42a159e88cdc01cafb1a10eaf43e3f7
sunny <saivamshidasari1432@gmail.com>
2024-12-10 22:11:42 -0500

    2st commit

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -5,8 +5,6 @@
  * Lahari ummadisetty-G01454186
  */
 package com.example.demo; // Replace with your package name
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,7 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allow all origins for testing purposes (use * for all)
         registry.addMapping("/**").allowedOrigins("*");
-        
         // Or, for more specific configuration, only allow a specific frontend origin:
         // registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
