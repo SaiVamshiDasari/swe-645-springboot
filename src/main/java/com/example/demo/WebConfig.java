acb36cc46ed491ed1b2eaba28fce3f4355979735
sunny <saivamshidasari1432@gmail.com>
2024-12-10 22:07:30 -0500

    2st commit

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -6,14 +6,12 @@
  */
 package com.example.demo; // Replace with your package name
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Allow all origins for testing purposes (use * for all)
